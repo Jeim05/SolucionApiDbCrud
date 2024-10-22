17f3728ccbcc20410c5110e4529011ea9f9bfa40
Jeim05 <lopezvegajeimmy@gmail.com>
2024-10-21 21:18:17 -0600

    Metodos crus controller

--- CapaWebApi/Controllers/EmpleadoController.cs
+++ CapaWebApi/Controllers/EmpleadoController.cs
@@ -23,5 +23,26 @@ namespace CapaWebApi.Controllers
             List<Empleado> empleados = await _empleadoData.Lista();
             return StatusCode(StatusCodes.Status200OK, empleados);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Crear([FromBody] Empleado objeto)
+        {
+            bool respuesta = await _empleadoData.Crear(objeto);
+            return StatusCode(StatusCodes.Status200OK, new {isSuccess = respuesta});
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Editar([FromBody] Empleado objeto)
+        {
+            bool respuesta = await _empleadoData.Editar(objeto);
+            return StatusCode(StatusCodes.Status200OK, new { isSuccess = respuesta });
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Eliminar(int id)
+        {
+            bool respuesta = await _empleadoData.Eliminar(id);
+            return StatusCode(StatusCodes.Status200OK, new { isSuccess = respuesta });
+        }
     }
 }
