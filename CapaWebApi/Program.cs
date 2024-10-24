3ca6ad6e2f84400f465b8e49cd8edf8bddb73fa7
Jeim05 <lopezvegajeimmy@gmail.com>
2024-10-23 19:47:00 -0600

    Agregar permisos del cliente

--- CapaWebApi/Program.cs
+++ CapaWebApi/Program.cs
@@ -1,6 +1,13 @@
 using CapaDatos;
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options=>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        policy => policy.WithOrigins("http://localhost:4200").
+                   AllowAnyMethod().
+                   AllowAnyHeader());
+});
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -19,6 +26,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowSpecificOrigin");
 
 app.UseAuthorization();
 
